ec8dda999d11ae45b7a0446685a32ca131294c5d
Kevin Williams <kevin8w@duck.com>
2024-03-17 16:51:04 +0800

    upgpkg(main/frp): 0.55.1
    Signed-off-by: Kevin Williams <kevin8w@duck.com>

--- packages/frp/build.sh
+++ packages/frp/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://gofrp.org/doc
 TERMUX_PKG_DESCRIPTION="A fast reverse proxy to expose a local server behind a NAT or firewall to the internet"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="2096779623 <admin@utermux.dev>"
-TERMUX_PKG_VERSION="0.54.0"
+TERMUX_PKG_VERSION="0.55.1"
 TERMUX_PKG_SRCURL=https://github.com/fatedier/frp/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=c09d8610b5eb02c0b1851459ace7751fe80a06b2373a6d5d40631a08e8ac64ae
+TERMUX_PKG_SHA256=c655fdf679aa94ec9abefc625b79bfb12aecf36011e23c207e322a0748c6c693
 TERMUX_PKG_REPLACES="frpc, frps"
 TERMUX_PKG_BREAKS="frpc, frps"
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -12,8 +12,20 @@ TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_make() {
 	termux_setup_golang
-	make frpc
-	make frps
+	if [ "$TERMUX_ARCH" = "arm" ]; then
+		export GOARCH=arm
+		export GOARM=7
+	elif [ "$TERMUX_ARCH" = "i686" ]; then
+		export GOARCH=386
+		export GO386=sse2
+	elif [ "$TERMUX_ARCH" = "aarch64" ]; then
+		export GOARCH=arm64
+	elif [ "$TERMUX_ARCH" = "x86_64" ]; then
+		export GOARCH=amd64
+	fi
+	CGO_ENABLED=0 GOOS=android go build -trimpath -ldflags "-s -w" -tags frps -o bin/frps ./cmd/frps
+	CGO_ENABLED=0 GOOS=android GOARCH=$TERMUX_ARCH go build -trimpath -ldflags "-s -w" -tags frpc -o bin/frpc ./cmd/frpc
+
 }
 
 termux_step_make_install() {
