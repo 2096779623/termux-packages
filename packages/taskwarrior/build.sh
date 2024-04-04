4cb1360d99c34960fd26ada113836a0046faf446
Kevin Williams <admin@utermux.dev>
2024-04-04 11:54:12 +0800

    upgpkg(main/taskwarrior): 3.0.0
    Signed-off-by: Kevin Williams <admin@utermux.dev>

--- packages/taskwarrior/build.sh
+++ packages/taskwarrior/build.sh
@@ -2,15 +2,16 @@ TERMUX_PKG_HOMEPAGE=https://taskwarrior.org
 TERMUX_PKG_DESCRIPTION="Utility for managing your TODO list"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.6.2
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=3.0.0
 TERMUX_PKG_SRCURL=https://github.com/GothenburgBitFactory/taskwarrior/releases/download/v${TERMUX_PKG_VERSION}/task-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=b1d3a7f000cd0fd60640670064e0e001613c9e1cb2242b9b3a9066c78862cfec
+TERMUX_PKG_SHA256=30f397081044f5dc2e5a0ba51609223011a23281cd9947ea718df98d149fcc83
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, libgnutls, libuuid, libandroid-glob"
 TERMUX_CMAKE_BUILD="Unix Makefiles"
 
 termux_step_pre_configure() {
+	termux_setup_rust
+
 	LDFLAGS+=" -landroid-glob"
 }
 
