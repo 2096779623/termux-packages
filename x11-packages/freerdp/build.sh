58156dd8c837351d8f488b114d706777c3bc3328
Kevin Williams <admin@utermux.dev>
2024-07-06 09:40:03 +0800

    upgpkg(x11/freerdp): 3.6.2
    Signed-off-by: Kevin Williams <admin@utermux.dev>

--- x11-packages/freerdp/build.sh
+++ x11-packages/freerdp/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://www.freerdp.com/
 TERMUX_PKG_DESCRIPTION="A free remote desktop protocol library and clients"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.11.7"
+TERMUX_PKG_VERSION="3.6.2"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_SRCURL=https://github.com/FreeRDP/FreeRDP/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=22dbeeaad065e93f152d70e04ec8eeab08aa32c406a96be64f252526623625a4
-TERMUX_PKG_DEPENDS="libandroid-shmem, libcairo, libjpeg-turbo, libusb, libwayland, libx11, libxcursor, libxdamage, libxext, libxfixes, libxi, libxinerama, libxkbcommon, libxkbfile, libxrandr, libxrender, libxv, openssl, pulseaudio, zlib"
+TERMUX_PKG_SHA256=e16260946a1d3289339bf158f335d9dddd9af43b96050b6b6fba287310df9924
+TERMUX_PKG_DEPENDS="libandroid-shmem, libcairo, libjpeg-turbo, libusb, libwayland, libx11, libxcursor, libxdamage, libxext, libxfixes, libxi, libxinerama, libxkbcommon, libxkbfile, libxrandr, libxrender, libxv, openssl, pulseaudio, zlib, libicu, sdl2"
 TERMUX_PKG_BUILD_DEPENDS="libwayland-cross-scanner, libwayland-protocols"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DANDROID_NO_TERMUX=OFF
@@ -21,10 +21,13 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DWITH_JPEG=ON
 -DWITH_OPENSSL=ON
 -DWITH_SERVER=ON
+-DWITH_KRB5=OFF
+-DWITH_SWSCALE=OFF
+-DWITH_FUSE=OFF
 "
 
 termux_pkg_auto_update() {
-	local latest_tag="$(termux_github_api_get_tag "${TERMUX_PKG_SRCURL}" latest-regex 2)"
+	local latest_tag="$(termux_github_api_get_tag "${TERMUX_PKG_SRCURL}" latest-regex 3)"
 	[[ -z "${latest_tag}" ]] && termux_error_exit "ERROR: Unable to get tag from ${TERMUX_PKG_SRCURL}"
 	termux_pkg_upgrade_version "${latest_tag}"
 }
