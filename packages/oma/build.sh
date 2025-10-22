4c15797ccb60ae68b54a6f2c869a5316c05d46d7
Kevin Williams <admin@utermux.dev>
2025-10-22 09:33:04 +0800

    enhance(main/oma): completions, manpages and conf
    Signed-off-by: Kevin Williams <admin@utermux.dev>

--- packages/oma/build.sh
+++ packages/oma/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="oma is an attempt at reworking APT's interface"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.22.3"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/AOSC-Dev/oma/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=55dbf68a329e3c4f9422b9b5ed0714071392f9f9e11aa17bdd84c0ac10339118
 TERMUX_PKG_DEPENDS="libnettle, apt"
@@ -27,5 +28,27 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
+	mkdir -p $TERMUX_PREFIX/share/man/man1
+	mkdir -p $TERMUX_PREFIX/etc/apt/apt.conf.d
+
 	install -Dm700 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/oma
+	install -m644 "${TERMUX_PKG_SRCDIR}/data/config/oma-debian.toml" "${TERMUX_PREFIX}/etc/oma.toml"
+	install -m644 "${TERMUX_PKG_SRCDIR}/data/apt.conf.d/50oma-debian.conf" "${TERMUX_PREFIX}/etc/apt/apt.conf.d/50oma.conf"
+}
+
+termux_step_post_make_install() {
+	install -Dm644 /dev/null "$TERMUX_PREFIX"/share/bash-completion/completions/oma
+	install -Dm644 /dev/null "$TERMUX_PREFIX"/share/zsh/site-functions/_oma
+	install -Dm644 /dev/null "$TERMUX_PREFIX"/share/fish/vendor_completions.d/oma.fish
+}
+
+termux_step_create_debscripts() {
+	cat <<-EOF >./postinst
+		#!${TERMUX_PREFIX}/bin/sh
+		COMPLETE=bash oma > ${TERMUX_PREFIX}/share/bash-completion/completions/oma
+		COMPLETE=zsh oma > ${TERMUX_PREFIX}/share/zsh/site-functions/_oma
+		COMPLETE=fish oma > ${TERMUX_PREFIX}/share/fish/vendor_completions.d/oma.fish
+		oma generate-manpages -p $TERMUX_PREFIX/tmp/
+		mv $TERMUX_PREFIX/tmp/man/*/*.1 $TERMUX_PREFIX/share/man/man1/
+	EOF
 }
